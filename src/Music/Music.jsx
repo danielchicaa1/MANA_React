689f95efafcf0a94c7792304e6c359bc2020095a
Daniel <felipe9321@hotmail.com>
2023-05-27 21:05:48 -0500

    se consume el API de musica

--- src/Music/Music.jsx
+++ src/Music/Music.jsx
@@ -1,56 +1,70 @@
 import { consultarCanciones } from "../services/servicioCanciones"
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react"
 import { ObtenerToken } from "../services/servicioCanciones"
-export function Music(){
+import './Music.css'
 
-      const [canciones,setCanciones] = useState(null)
-      const[estacargando,setEstacargando]=useState(true)
-      const[token,setToken]= useState(null)
+export function Music() {
 
-      
+      const [canciones, setCanciones] = useState(null)
+      const [estacargando, setEstacargando] = useState(true)
+      const [token, setToken] = useState(null)
 
-      useEffect(function(){
-            ObtenerToken().then(respuesta=>{
+
+
+      useEffect(function () {
+            ObtenerToken().then(respuesta => {
                   setToken(respuesta.access_token)
             })
-      },[])
+      }, [])
 
 
-      useEffect(function(){
-            if(token){
-                  consultarCanciones(token).then(function(respuesta){
+      useEffect(function () {
+            if (token) {
+                  consultarCanciones(token).then(function (respuesta) {
                         console.log(respuesta);
                         setCanciones(respuesta.tracks);
                         setEstacargando(false)
                   })
             }
-            
-      },[token]);
 
-      if (estacargando){
-            return(
+      }, [token]);
+
+      if (estacargando) {
+            return (
                   <>
-                  <h1>ESTA CARGANDO EL CONTENIDO</h1>
+                        <h1>ESTA CARGANDO EL CONTENIDO</h1>
                   </>
             )
-            
-      }else{
-            return(
+
+      } else {
+            return (
                   <>
-                  {
-                        canciones.map(function(cancion){
-                              return(
-                                    <div>
-                                    <audio controls src={cancion.preview_url}></audio>
-                                    <h1>{ cancion.popularity} </h1>
-                                    <p>{cancion.name}</p>
-                                    </div>
-                              )
-                        })
-                  }
+                  <div className="fondo">
+                        <div className="container my-5">
+                              <div className="row row-cols-1 row-cols-md-2 g-3">
+                                    {
+                                          canciones.map(function (cancion) {
+                                                return (
+                                                      
+                                                      <div className="col">
+                                                            <div className="card bg-dark border-info h-100 shadow p-3">
+                                                            <h2 className="text-center text-white ">{cancion.name}</h2>
+                                                            <h3>Popularidad:{cancion.popularity}</h3>
+                                                            <img src={cancion.album.images[1].url} alt="" />
+                                                            <audio controls  src={cancion.preview_url}></audio>
+                                                            </div>
+                                                            
+
+                                                      </div>
+                                                )
+                                          })
+                                    }
+                              </div>
+                        </div>
+                  </div>
                   </>
             )
       }
 
-      
+
 }
